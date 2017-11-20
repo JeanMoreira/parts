06f2b1a5bdb32478deb6aacb176a57aa57aa6c52
Jean Moreira <jeanmoreira@localJean.local>
2017-11-20 17:51:27 -0200

    Finalizacao do anuncio do administrador e inicio da criacao do anuncio do member

---
+++ app/controllers/site/profile/parts_controller.rb
@@ -0,0 +1,84 @@
+require 'parts_controller/part_service.rb'
+class Site::Profile::PartsController < Site::ProfileController
+    before_action :set_part, only: [:edit, :update, :destroy]
+    
+      # GET /parts
+      # GET /parts.json
+      def index
+        puts("-------------")
+        puts(current_member)
+        @parts = self.get_part_by_member(current_member)
+        @categories = Category.all
+      end
+    
+      # GET /parts/1
+      # GET /parts/1.json
+      def show
+      end
+    
+      # GET /parts/new
+      def new
+        @part = Part.new
+      end
+    
+      # GET /parts/1/edit
+      def edit
+      end
+    
+      # POST /parts
+      # POST /parts.json
+      def create
+        @part = Part.new(part_params)
+    
+        respond_to do |format|
+          if @part.save
+            format.html { redirect_to site_profile_parts_path, notice: 'Part was successfully created.' }
+            format.json { render :show, status: :created, location: @part }
+          else
+            format.html { render :new }
+            format.json { render json: @part.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+    
+      # PATCH/PUT /parts/1
+      # PATCH/PUT /parts/1.json
+      def update
+        respond_to do |format|
+          if @part.update(part_params)
+            format.html { redirect_to site_profile_parts_path, notice: 'Part was successfully updated.' }
+            format.json { render :show, status: :ok, location: @part }
+          else
+            format.html { render :edit }
+            format.json { render json: @part.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+    
+      # DELETE /parts/1
+      # DELETE /parts/1.json
+      def destroy
+        @part.destroy
+        respond_to do |format|
+          format.html { redirect_to site_profile_parts_path, notice: 'Part was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+      end
+    
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_part
+          @part = Part.find(params[:id])
+        end
+    
+        # Never trust parameters from the scary internet, only allow the white list through.
+        def part_params
+          #apos a verificacao ele vai ignorar o car_id
+          params.require(:part).permit(:car_id, :description, :category_id,:picture)
+        end
+    
+        #Verificar se o id do car_id esta em branco e informa 
+        def car_id_blank?
+          params[:part][:car_id].blank?
+        end
+end
---
+++ test/controllers/site/profile/parts_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Site::Profile::PartsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
